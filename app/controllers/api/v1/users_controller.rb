dd0b867482bba095ef94bdd50368e88a4d051a86
Manuel Grullon <astermanuelg@gmail.com>
2018-05-25 14:08:19 -0400

    show and index initial set up for User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,29 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :find_user, only: [:show, :update, :delete]
+      def index
+        render json: User.all
+      end
+
+      def show
+        render json: @user
+      end
+
+      def create
+      end
+
+      def update
+      end
+
+      def delete
+      end
+
+      private
+
+      def find_user
+        @user = User.find(params[:id])
+      end
     end
   end
 end
