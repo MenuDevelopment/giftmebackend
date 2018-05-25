fe238e3e683ac3f52f74c9988fefd7f37101729c
Manuel Grullon <astermanuelg@gmail.com>
2018-05-25 10:21:47 -0400

    testing has many relationships

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,2 +1,5 @@
 class Gift < ApplicationRecord
+  belongs_to :creator, class_name: "User"
+  has_many :pledges
+  has_many :users, through: :pledges
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :pledges
+  has_many :gifts, through: :pledges
+  has_many :created_gifts, class_name: "Gift"
 end
