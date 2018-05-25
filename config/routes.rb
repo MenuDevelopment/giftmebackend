a555e316d5542386b4d57e89ad1d0d06868d606c
Manuel Grullon <astermanuelg@gmail.com>
2018-05-25 10:41:03 -0400

    namespacing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :gifts
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :gifts
+        resources :pledges
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
