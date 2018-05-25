9fd457c893e12cb865273ebad8f8a2ede7e02643
Manuel Grullon <astermanuelg@gmail.com>
2018-05-25 14:01:10 -0400

    seed file done 🌱

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users = []
 
 20.times do
-  User.create({
+  users.push(User.create({
     username: Faker::Internet.user_name,
     email: Faker::Internet.email,
     password: Faker::Internet.password,
     payment: Faker::Bitcoin.address
-    })
+    }))
 end
+gifts = []
+10.times do
+  gifts.push(Gift.create({
+    item: Faker::Commerce.product_name,
+    price: Faker::Commerce.price,
+    user_id: users.sample.id
+    }))
+end
+
+10.times do
+  Pledge.create({
+    user: users.sample,
+    gift: gifts.sample,
+    amount: 1.50
+    })
+  end
